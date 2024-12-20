ddb3c4cf422ec9fd94a418c1bff6114c57fb4c76
HarisQurx <haris.qurx@gmail.com>
2024-12-20 23:05:25 +0100

    Add project files and update .gitignore

---
+++ TaskManagerApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TaskManagerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Task Management System!");
+            Console.WriteLine("=====================================");
+            Console.WriteLine("This system will allow you to manage users and tasks effectively.");
+        }
+    }
+}
